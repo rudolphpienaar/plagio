bd7c2f4a5c02cf2c58d7b4abebb22e0c34a493d0
Nicolas Rannou <huko.rannou@gmail.com>
2013-11-14 13:46:06 -0500

    some tunning

--- index.php
+++ index.php
@@ -1,7 +1,34 @@
 <?php
 
-$content = file_get_contents("templates/home.html");
+if(isset($_GET['IECheck'])){
+  $iecheck = $_GET['IECheck'];
+}
+else{
+  $iecheck = 1;
+}
 
+$IETag = '';
+if($iecheck){
+  $IETag = '
+<!--[if IE]>
+<script>
+var tmpFunc = window.onload;
+window.onload =  function() {
+  document.getElementById("open-overlay").click();
+  tmpFunc();
+}
+</script>
+<![endif]-->
+';
+}
+
+$content = file_get_contents("templates/home.html");
+// add overlay
+$content = str_replace('${OVERLAY}', file_get_contents("templates/overlay.html"), $content);
+// add IE Test
+$content = str_replace('${IETAG}', $IETag, $content);
+// add IECheck
+$content = str_replace('${IECHECK}', $iecheck, $content);
 // add footer
 $content = str_replace('${FOOTER}', file_get_contents("templates/footer.html"), $content);
 
--- preparingForYourStudyVisit.php
+++ preparingForYourStudyVisit.php
@@ -1,7 +1,16 @@
 <?php
 
+if(isset($_GET['IECheck'])){
+  $iecheck = $_GET['IECheck'];
+}
+else{
+  $iecheck = 1;
+}
+
 $content = file_get_contents("templates/preparingForYourStudyVisit.html");
 
+// add IECheck
+$content = str_replace('${IECHECK}', $iecheck, $content);
 // add footer
 $content = str_replace('${FOOTER}', file_get_contents("templates/footer.html"), $content);
 
